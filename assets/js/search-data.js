a5789f20a2ef059f0ed7fe03728da3c3cf2ed50f
sonsoleslp <sonsoleslp@users.noreply.github.com>
2025-01-13 21:55:26 +0000

    Deploying to gh-pages from @ mohsaqr/mohsaqr.github.io@8dfccccd47d50fe3cce52cff4ec6ebb951c41591 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,7 +52,7 @@ ninja.data = [{
             window.location.href = "/Interests/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {

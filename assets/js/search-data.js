271c56a17248ccab8ccee25a267ecd98b8e295da
sonsoleslp <sonsoleslp@users.noreply.github.com>
2025-01-13 21:54:20 +0000

    Deploying to gh-pages from @ mohsaqr/mohsaqr.github.io@ed6a94514122b6b3a90b0c278527af9f5847b572 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/blog/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {

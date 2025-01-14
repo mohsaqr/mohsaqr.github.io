4944aa8b3cea0305c5deb3da6af24e3b1929ead9
mohsaqr <hamada@saqr.me>
2025-01-14 12:48:24 +0000

    Deploying to gh-pages from @ mohsaqr/mohsaqr.github.io@7d2ed35d0ef0bb328768cd0db080516253f14316 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,16 +438,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+    },{id: "news-transition-network-analysis-available",
+          title: 'Transition network analysis available',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-ramy-has-completed-my-phd-in-computer-science-from-the-learning-analytics-unit-at-the-school-of-computing-university-of-eastern-finland",
+          title: 'Ramy has completed my PhD in Computer Science from the Learning analytics unit...',
+          description: "",
+          section: "News",},{id: "news-just-got-a-turksih-chapter-published-and-it-feels-great-having-my-research-in-other-languages",
+          title: 'Just got a Turksih chapter published and it feels great having my research...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',

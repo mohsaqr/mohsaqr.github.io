aa7ca0a0f4ffc92c100e75c5f2b9fd15ea95f204
mohsaqr <hamada@saqr.me>
2025-01-15 12:27:49 +0000

    Deploying to gh-pages from @ mohsaqr/mohsaqr.github.io@cfeedbb1cf25eba0f7e2cd33bc1e77fcfdf2a774 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Interests/";
           },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "As an academic, teaching and learning are central to everyday job. From teaching formal courses, to supervision tasks to mentoring students. Here, I describe my teaching and assessment philosophy. I also list my courses and how you can enrol in them. If you are looking for a PhD supervision or",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "dropdown-publications",
               title: "publications",
               description: "",
